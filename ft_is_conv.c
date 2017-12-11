b83ae83b60c811fd0cbdd07839ced74bba1f6176
Nicolas VERGNAC <nvergnac@e1r8p3.42.fr>
2017-12-11 17:31:44 +0100

    norme et gestion des leak sur ft_is_conv.c

--- ft_is_conv.c
+++ ft_is_conv.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_conv.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pclement <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nvergnac <nvergnac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/08 19:24:58 by pclement          #+#    #+#             */
-/*   Updated: 2017/12/11 14:44:39 by pclement         ###   ########.fr       */
+/*   Created: 2017/12/11 17:13:14 by nvergnac          #+#    #+#             */
+/*   Updated: 2017/12/11 17:14:49 by nvergnac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_is_type_array(char *format, int start, t_info *info_struct)
+int		ft_type(char *format, int st, t_info *inf)
 {
 	int		j;
 	char	*type_str;
@@ -21,9 +21,9 @@ int		ft_is_type_array(char *format, int start, t_info *info_struct)
 	j = 0;
 	while (type_str[j] != 0)
 	{
-		if (format [start] == type_str[j])
+		if (format[st] == type_str[j])
 		{
-			info_struct->type = type_str[j];
+			inf->type = type_str[j];
 			return (1);
 		}
 		j++;
@@ -31,151 +31,185 @@ int		ft_is_type_array(char *format, int start, t_info *info_struct)
 	return (0);
 }
 
-// pour les modifier pb car met un caractere dans une string sans conclure
-int		ft_check_special_modifier(char *format,int start, char modifier_str, t_info *info_struct)
+int		ft_chk_spec_mdf(char *format, int st, char mdf_str, t_info *info)
 {
-	if (format[start] == modifier_str && (modifier_str == 'h' || modifier_str == 'l'))
+	if (format[st] == mdf_str && (mdf_str == 'h' || mdf_str == 'l'))
 	{
-		if (format[start + 1] == modifier_str)
+		if (format[st + 1] == mdf_str)
 		{
-			if (modifier_str == 'h')
-				info_struct->modifier = "hh";
+			if (mdf_str == 'h')
+				info->mdf = "hh";
 			else
-				info_struct->modifier = "ll";
+				info->mdf = "ll";
 			return (2);
 		}
 		else
 		{
-			info_struct->modifier = ft_strnew(1);
-			info_struct->modifier[0] = modifier_str;
+			info->mdf = ft_strnew(1);
+			info->mdf[0] = mdf_str;
 			return (1);
 		}
 	}
 	return (0);
 }
 
-int		ft_is_modif_array(char *format, int start, t_info *info_struct)
+int		ft_mdf(char *format, int st, t_info *inf)
 {
 	int		j;
-	char	*modifier_str;
+	char	*mdf_str;
 
-	modifier_str = "hljz";
+	mdf_str = "hljz";
 	j = 0;
-	while (modifier_str[j] != 0)
+	while (mdf_str[j] != 0)
 	{
-		if (format[start] == modifier_str[j] && modifier_str[j] != 'h' && modifier_str[j] != 'l')
+		if (format[st] == mdf_str[j] && mdf_str[j] != 'h'
+				&& mdf_str[j] != 'l')
 		{
-			info_struct->modifier = ft_strnew(1);
-			info_struct->modifier[0] = modifier_str[j];
+			inf->mdf = ft_strnew(1);
+			inf->mdf[0] = mdf_str[j];
 			return (1);
 		}
-		if (ft_check_special_modifier(format, start, modifier_str[j], info_struct) > 0)
-			return (ft_check_special_modifier(format, start, modifier_str[j], info_struct));
+		if (ft_chk_spec_mdf(format, st, mdf_str[j], inf) > 0)
+			return (ft_chk_spec_mdf(format, st, mdf_str[j], inf));
 		j++;
 	}
 	return (0);
 }
 
-int		ft_is_accuracy_array(char *format, int start, t_info *info_struct)
+char	*ft_append_acc_char(char format, int k, char *tmp)
 {
-	int		k;
-	char	*tmp = ".";
 	char	*buf;
 
-	if (!(buf = ft_strnew(1)))
+	buf = ft_strdup(tmp);
+	free(tmp);
+	if (!(tmp = ft_strnew(ft_strlen(buf) + 1)))
 		exit(0);
-	if (format[start] != '.')
+	ft_strcpy(tmp, buf);
+	free(buf);
+	tmp[k] = format;
+	return (tmp);
+}
+
+int		ft_acc(char *format, int st, t_info *inf)
+{
+	int		k;
+	char	*tmp;
+
+	if (format[st] != '.')
 		return (0);
+	if (!(tmp = ft_strnew(1)))
+		exit(0);
+	tmp[0] = '.';
 	k = 1;
-	while (format[start + k] >= '0' && format[start + k] <= '0' + 9)
-	{	
-		buf[0] = format[start + k];
-		if (!(tmp = ft_strjoin(tmp, buf)))
-			exit (0);
-		k++;
-	}
-	if (!(info_struct->accuracy = ft_strdup(tmp)))
-		exit (0);
-	return (k);
-}
-
-int		ft_is_size_array(char *format, int start, t_info *info_struct)
-{
-	int		k;
-	char	*tmp;
-	char	*buf;
-
-	k = 0;
-	tmp = ft_strnew(0);
-	if (!(buf = ft_strnew(1)))
-		exit(0);
-	while (format[start + k] >= '0' && format[start + k] <= '0' + 9)
-	{	
-		buf[0] = format[start + k];
-		if (!(tmp = ft_strjoin(tmp, buf)))
-			exit (0);
-		k++;
-	}
-	if (!(info_struct->size = ft_strdup(tmp)))
-		exit (0);
-	return (k);
-}
-
-int		ft_is_flag_array(char *format, int start, t_info *info_struct)
-{
-	int		k;
-	int		j;
-	char	*tmp;
-	char	*buf;
-	char	*flag_str;
-	int		switch_flag;
-
-	flag_str = "# +-";
-	k = 0;
-	if (!(tmp = ft_strnew(0)))
-		exit(0);
-	if (!(buf = ft_strnew(1)))
-		exit(0);
-	switch_flag = 0;
-	while (format[start + k] != 0 && switch_flag == 0)
+	while (format[st + k] >= '0' && format[st + k] <= '9')
 	{
-		j = 0;
-		while (flag_str[j] != 0)
-		{
-			switch_flag = 1;
-			if (flag_str[j] == format[start + k])
-			{
-				buf[0] = format[start + k];
-				if (!(tmp = ft_strjoin(tmp, buf)))
-					exit (0);
-				switch_flag = 0;
-				break;
-			}
-			j++;
-		}
+		if (!(tmp = ft_append_acc_char(format[st + k], k, tmp)))
+			exit(0);
 		k++;
 	}
-	if (!(info_struct->flags = ft_strdup(tmp)))
-		exit (0);
-
-	return (k - 1);
+	if (!(inf->acc = ft_strdup(tmp)))
+		exit(0);
+	free(tmp);
+	return (k);
 }
 
-int		ft_is_conv(char *format, int start, int flag, t_info *info_struct_ptr)
+char	*ft_append_size_char(char format, int k, char *tmp)
 {
-	if (format[start] != '%' && flag == 0)
+	char	*buf;
+
+	buf = ft_strdup(tmp);
+	free(tmp);
+	if (!(tmp = ft_strnew(ft_strlen(buf) + 1)))
+		exit(0);
+	ft_strcpy(tmp, buf);
+	free(buf);
+	tmp[k] = format;
+	return (tmp);
+}
+
+int		ft_size(char *format, int st, t_info *inf)
+{
+	int		k;
+	char	*tmp;
+
+	k = 0;
+	if (!(tmp = ft_strnew(1)))
+		exit(0);
+	while (format[st + k] >= '0' && format[st + k] <= '9')
+	{
+		if (!(tmp = ft_append_size_char(format[st + k], k, tmp)))
+			exit(0);
+		k++;
+	}
+	if (!(inf->size = ft_strdup(tmp)))
+		exit(0);
+	free(tmp);
+	return (k);
+}
+
+char	*ft_apd_flg_chr(char format, t_flag flag)
+{
+	char	*buf;
+
+	if (flag.k == 0)
+	{
+		if (!(flag.tmp = ft_strnew(1)))
+			exit(0);
+	}
+	buf = ft_strdup(flag.tmp);
+	free(flag.tmp);
+	if (!(flag.tmp = ft_strnew(ft_strlen(buf) + 1)))
+		exit(0);
+	ft_strcpy(flag.tmp, buf);
+	free(buf);
+	flag.tmp[flag.k] = format;
+	return (flag.tmp);
+}
+
+int		ft_flag(char *format, int st, t_info *inf)
+{
+	t_flag		flag;
+
+	flag.flag_str = "# +-";
+	flag.k = 0;
+	flag.switch_flag = 0;
+	while (format[st + flag.k] != 0 && flag.switch_flag == 0)
+	{
+		flag.j = 0;
+		while (flag.flag_str[flag.j] != 0)
+		{
+			flag.switch_flag = 1;
+			if (flag.flag_str[flag.j++] == format[st + flag.k])
+			{
+				if (!(flag.tmp = ft_apd_flg_chr(format[st + flag.k], flag)))
+					exit(0);
+				flag.switch_flag = 0;
+				break ;
+			}
+		}
+		flag.k++;
+	}
+	if (!(inf->flags = ft_strdup(flag.tmp)))
+		exit(0);
+	free(flag.tmp);
+	return (flag.k - 1);
+}
+
+int		ft_conv(char *format, int st, int flag, t_info *inf_ptr)
+{
+	if (format[st] != '%' && flag == 0)
 		return (0);
 	if (flag == 0)
-		start++;
-	if (ft_is_type_array(format, start, info_struct_ptr) >= 1 && flag <= 5)
+		st++;
+	if (ft_type(format, st, inf_ptr) >= 1 && flag <= 5)
 		return (1);
-	if (ft_is_modif_array(format, start, info_struct_ptr) >= 1 && flag <= 4)
-		return (ft_is_conv(format, start + ft_is_modif_array(format, start, info_struct_ptr), 5, info_struct_ptr));
-	if (ft_is_accuracy_array(format, start, info_struct_ptr) >= 1 && flag <= 3)
-		return (ft_is_conv(format, start + ft_is_accuracy_array(format, start, info_struct_ptr), 4, info_struct_ptr));
-	if (ft_is_size_array(format, start, info_struct_ptr) >= 1 && flag <= 2)
-		return (ft_is_conv(format, start + ft_is_size_array(format, start, info_struct_ptr), 3, info_struct_ptr));
-	if (ft_is_flag_array(format, start, info_struct_ptr) >= 1 && flag <= 1)
-		return (ft_is_conv(format, start + ft_is_flag_array(format, start, info_struct_ptr), 2, info_struct_ptr));
+	if (ft_mdf(format, st, inf_ptr) >= 1 && flag <= 4)
+		return (ft_conv(format, st + ft_mdf(format, st, inf_ptr), 5, inf_ptr));
+	if (ft_acc(format, st, inf_ptr) >= 1 && flag <= 3)
+		return (ft_conv(format, st + ft_acc(format, st, inf_ptr), 4, inf_ptr));
+	if (ft_size(format, st, inf_ptr) >= 1 && flag <= 2)
+		return (ft_conv(format, st + ft_size(format, st, inf_ptr), 3, inf_ptr));
+	if (ft_flag(format, st, inf_ptr) >= 1 && flag <= 1)
+		return (ft_conv(format, st + ft_flag(format, st, inf_ptr), 2, inf_ptr));
 	return (0);
 }
